227aeefa0607dddf4615db50e3c86006ea55f4ce
Eliot <saltycrane@gmail.com>
2019-06-17 18:19:44 -0700

    add emotion

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,14 @@
-import React from "react";
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core";
 
-const Home = () => <div>Home</div>;
+const Home = () => (
+  <div
+    css={css`
+      color: #ccc;
+    `}
+  >
+    Home
+  </div>
+);
 
 export default Home;
